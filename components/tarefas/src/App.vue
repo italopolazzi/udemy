2778967646f54d2860fd087b9a7b36dd67b216c4
Ítalo Polazzi Ferreira <itpzzi@gmail.com>
2019-08-27 23:36:22 -0300

    add: filtros por estado da tarefa

--- components/tarefas/src/App.vue
+++ components/tarefas/src/App.vue
@@ -5,29 +5,47 @@
     <h1 class="dark">Tarefas</h1>
 
     <BarraDeProgresso :tarefas="tarefas" />
-    <ListaDeTarefas :tarefas="tarefas" />
+    <ControlesDeEstado :botoes="botoes" @filtroDeEstadoSelecionado="filtrarPorEstado" />
+    <ListaDeTarefas :tarefas="tarefas_filtradas" />
   </div>
 </template>
 
 <script>
 import BarraDeProgresso from "@/components/BarraDeProgresso.vue";
+import ControlesDeEstado from "@/components/ControlesDeEstado.vue";
 import NovaTarefa from "@/components/NovaTarefa.vue";
 import ListaDeTarefas from "@/components/ListaDeTarefas.vue";
 
 export default {
   components: {
     BarraDeProgresso,
+    ControlesDeEstado,
     NovaTarefa,
     ListaDeTarefas
   },
+  computed: {
+    tarefas_filtradas() {
+      const filtro = this.botoes[this.filtro_selecionado].filtro;
+      return this.tarefas.filter(filtro);
+    }
+  },
   data() {
     return {
-      tarefas: []
+      tarefas: [],
+      filtro_selecionado: 0,
+      botoes: [
+        { nome: "Todos", filtro: item => item },
+        { nome: "Concluídos", filtro: item => item.status },
+        { nome: "Não concluídos", filtro: item => !item.status }
+      ]
     };
   },
   methods: {
     adicionarNovaTarefa(valor) {
       this.tarefas.push({ legenda: valor, status: false });
+    },
+    filtrarPorEstado(index) {
+      this.filtro_selecionado = index;
     }
   },
   watch: {
---
+++ components/tarefas/src/components/ControlesDeEstado.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="controles-de-estado">
+    <button v-for="(botao, index) in botoes" :key="index" @click="$emit('filtroDeEstadoSelecionado', index)">{{botao.nome}}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    botoes: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<style>
+</style>
