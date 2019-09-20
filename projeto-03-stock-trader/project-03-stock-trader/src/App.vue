9f2b4cd672aa2c3a39002bddcd19affcd797dc42
Ítalo Polazzi Ferreira <itpzzi@gmail.com>
2019-09-19 22:43:10 -0300

    add: GlobalMessages como um componente

--- projeto-03-stock-trader/project-03-stock-trader/src/App.vue
+++ projeto-03-stock-trader/project-03-stock-trader/src/App.vue
@@ -1,22 +1,18 @@
 <template>
   <div id="app">
+    <GlobalMessages />
     <template v-if="is_loading">
       <BubbleLoader />
     </template>
     <template v-else>
-      <!-- <div class="global-messages" v-if="global_messages.length">
-        <ul>
-          <li v-for="(message, index) in global_messages" :key="index">{{index}}-{{message.text}}</li>
-        </ul>
-      </div> -->
       <LeftDrawer />
-
       <router-view />
     </template>
   </div>
 </template>
 
 <script>
+import GlobalMessages from "@/components/partials/GlobalMessages";
 import LeftDrawer from "@/components/partials/LeftDrawer";
 import BubbleLoader from "@/components/partials/BubbleLoader";
 import { mapGetters } from "vuex";
@@ -24,6 +20,7 @@ import { mapGetters } from "vuex";
 export default {
   name: "app",
   components: {
+    GlobalMessages,
     LeftDrawer,
     BubbleLoader
   },
@@ -31,14 +28,7 @@ export default {
     this.$store.dispatch("loadAll", this);
   },
   computed: {
-    ...mapGetters(["global_messages", "is_loading"])
-  },
-  watch: {
-    global_messages() {
-      setTimeout(() => {
-        this.$store.state.global.messages.shift();
-      }, 5000);
-    }
+    ...mapGetters(["is_loading"])
   }
 };
 </script>
